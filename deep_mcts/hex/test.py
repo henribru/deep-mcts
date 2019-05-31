4f739524323b92db53c436e31d59eeed1faf9912
henribru <6639509+henribru@users.noreply.github.com>
2019-05-31 13:54:55 +0200

    Fix incorrect imports

--- deep_mcts/hex/test.py
+++ deep_mcts/hex/test.py
@@ -1,15 +1,15 @@
 from __future__ import annotations
 
 from deep_mcts import train
-from deep_mcts.hex import HexManager
-from deep_mcts.hex import ConvolutionalHexANET
+from deep_mcts.hex.game import HexManager
+from deep_mcts.hex.convolutionalnet import ConvolutionalHexNet
 
 
 def hex_simulator(
     grid_size: int, num_actual_games: int, num_search_games: int, save_interval: int
 ) -> None:
     hex = HexManager(grid_size)
-    anet = ConvolutionalHexANET(grid_size)
+    anet = ConvolutionalHexNet(grid_size)
     for _ in train.train(
         anet, hex, num_actual_games, num_search_games, save_interval
     ):
@@ -20,6 +20,6 @@ if __name__ == "__main__":
     num_actual_games = 10
     hex_simulator(4, num_actual_games, num_search_games=100, save_interval=100)
     # state_manager = HexManager(4)
-    # print(topp([ConvolutionalHexANET(4).sampling_policy, ConvolutionalHexANET(4).sampling_policy], 100, state_manager))
-    # agents = [ConvolutionalHexANET.from_path(f"anet-{i}.pth", 4).sampling_policy for i in range(0, 110, 10)]
+    # print(topp([ConvolutionalHexNet(4).sampling_policy, ConvolutionalHexNet(4).sampling_policy], 100, state_manager))
+    # agents = [ConvolutionalHexNet.from_path(f"anet-{i}.pth", 4).sampling_policy for i in range(0, 110, 10)]
     # import pprint;pprint.pprint(topp(agents, 25, state_manager))
